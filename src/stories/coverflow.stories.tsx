85b4f6c0d8d0ed54cf96e80d80462571da901ca6
David Friar <david.friar@cognifide.com>
2022-04-12 19:36:59 +0100

    storybook cleanup

--- src/stories/coverflow.stories.tsx
+++ src/stories/coverflow.stories.tsx
@@ -11,87 +11,39 @@ export default {
   },
   argTypes: {
     numberOfItems: {
-      description:
-        "Size of the array of items to set up for this test. (This is not a property of the Coverflow component).",
-      control: {
-        type: "number",
-        min: 1,
-        max: 500,
-      },
+      description: `Size of the array of items to set up for this test.
+      (This is part of the test fixture, not a property of the Coverflow component).`,
+      control: { type: "number", min: 1, max: 500 },
     },
     items: {
       control: false,
     },
     sideItems: {
-      control: {
-        type: "range",
-        min: 1,
-        max: 50,
-        step: 1,
-      },
+      control: { type: "range", min: 1, max: 50, step: 1 },
     },
     startPosition: {
-      control: {
-        type: "number",
-        min: 1,
-        max: 500,
-      },
+      control: { type: "number", min: 1, max: 500 },
     },
     scale: {
-      control: {
-        type: "range",
-        min: 0.1,
-        max: 3,
-        step: 0.1,
-      },
+      control: { type: "range", min: 0.1, max: 3, step: 0.1 },
     },
     scaleRatio: {
-      control: {
-        type: "range",
-        min: 0.1,
-        max: 3,
-        step: 0.1,
-      },
+      control: { type: "range", min: 0.1, max: 3, step: 0.1 },
     },
     angle: {
-      control: {
-        type: "range",
-        min: 0,
-        max: 90,
-        step: 1,
-      },
+      control: { type: "range", min: 0, max: 90, step: 1 },
     },
     gap: {
-      control: {
-        type: "range",
-        min: 1,
-        max: 100,
-        step: 5,
-      },
+      control: { type: "range", min: 1, max: 100, step: 5 },
     },
     centreGap: {
-      control: {
-        type: "range",
-        min: 1,
-        max: 100,
-        step: 1,
-      },
+      control: { type: "range", min: 1, max: 100, step: 1 },
     },
     perspective: {
-      control: {
-        type: "range",
-        min: 0,
-        max: 2000,
-        step: 10,
-      },
+      control: { type: "range", min: 0, max: 2000, step: 10 },
     },
     depth: {
-      control: {
-        type: "range",
-        min: 0,
-        max: 1000,
-        step: 10,
-      },
+      control: { type: "range", min: 0, max: 1000, step: 10 },
     },
     showReflections: {
       control: {
@@ -99,20 +51,10 @@ export default {
       },
     },
     dragThreshold: {
-      control: {
-        type: "range",
-        min: 0,
-        max: 250,
-        step: 5,
-      },
+      control: { type: "range", min: 0, max: 250, step: 5 },
     },
     dragSpeed: {
-      control: {
-        type: "range",
-        min: 0,
-        max: 4,
-        step: 0.25,
-      },
+      control: { type: "range", min: 0, max: 4, step: 0.25 },
     },
     clickableSide: {
       control: {
